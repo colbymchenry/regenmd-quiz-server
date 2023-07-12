54544f076b1c6dd4d87cba3d2c6bf9c132d02fa2
Colby McHenry <me@colbymchenry.com>
2023-07-12 17:47:42 -0500

    Update cors

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 // enable JSON body parser
 app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    optionsSuccessStatus: 200
+}));
 
 app.get('/', (req, res) => {
     res.json({
